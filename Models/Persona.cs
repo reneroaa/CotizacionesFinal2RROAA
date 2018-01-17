0b538145ecaaefa6655a498879313e7c6dc74766
Rene Roa <reneroaa@gmail.com>
2018-01-17 02:51:16 -0300

    Se agrega al modelo clase Persona

---
+++ Models/Persona.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Archivo donde se definen las clases del Dominio del problema.
+/// </summary>
+namespace Cotizaciones.Models
+{
+
+    /// <summary>
+    /// Clase que representa a una persona en el Sistema.
+    /// </summary>
+    public class Persona
+    {
+        public String Rut { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Paterno { get; set; }
+
+        public string Materno { get; set; } 
+
+	}
+    
+}
