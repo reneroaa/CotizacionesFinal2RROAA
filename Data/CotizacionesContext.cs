c4fa26cf97aed85ea59ed1e318b09d150466f3c6
Rene Roa <reneroaa@gmail.com>
2018-01-17 02:52:17 -0300

    Se agrega contexto ORM

---
+++ Data/CotizacionesContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Cotizaciones.Models;
+
+namespace Cotizaciones.Data {
+    public class CotizacionesContext : DbContext
+    {
+        public CotizacionesContext(DbContextOptions<CotizacionesContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Utilizacion de SQLite como backend
+            optionsBuilder.UseSqlite("Data Source=cotizaciones.db");
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+
+    }
+}
