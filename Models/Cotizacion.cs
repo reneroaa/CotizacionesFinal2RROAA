b99e1de157f9316658ac9d073c1a191c123647fd
Rene Roa <reneroaa@gmail.com>
2018-01-17 03:29:54 -0300

    Se agrega clase Cotizacion

---
+++ Models/Cotizacion.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+/// <summary>
+/// Archivo donde se definen las clases del Dominio del problema.
+/// </summary>
+namespace Cotizaciones.Models
+{
+
+    /// <summary>
+    /// Clase que representa a una cotizacion en el Sistema.
+    /// </summary>
+    /// <remarks>
+    /// Esta clase pertenece al modelo del Dominio y posee las siguientes restricciones:
+    /// - No permite valores null en sus atributos.
+    /// </remarks>
+    public class Cotizacion
+    {
+        public int ID { get; set; }
+        public string nombreCliente{get ; set; }
+        public string descripcionCotizacion{get; set; }
+        public int valor{get; set; }
+        public string estado{get; set; }
+        public DateTime fechaCotizacion {get; set; }
+        public DateTime fechaValidez{get; set; }
+        public String PersonaRut {get;set; }
+        [ForeignKey("PersonaRut")]
+        public Persona persona {get; set;}
+
+    }
+}
