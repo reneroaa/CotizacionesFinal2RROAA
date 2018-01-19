4aeb12777ac844ed41d95d16a0e0a3be7bd3ba41
Rene Roa <reneroaa@gmail.com>
2018-01-19 04:38:14 -0300

    Modificación de vista Cotizacion/Detail

--- CotizacionesFinal2RROAA/Models/Persona.cs
+++ CotizacionesFinal2RROAA/Models/Persona.cs
@@ -23,6 +23,39 @@ namespace Cotizaciones.Models
 
 
 
+        public Persona validate(){
+            //validacion de rut
+            if(!validarRut(this.Rut)){
+                throw new ArgumentException("RUT no valido");
+            }
+            return this;
+        }
+
+        public bool validarRut(string rut ) {
+                    
+            bool validacion = false;
+            try {
+                rut =  rut.ToUpper();
+                rut = rut.Replace(".", "");
+                rut = rut.Replace("-", "");
+                int rutAux = int.Parse(rut.Substring(0, rut.Length - 1));
+        
+                char dv = char.Parse(rut.Substring(rut.Length - 1, 1));
+        
+                int m = 0, s = 1;
+                for (; rutAux != 0; rutAux /= 10) {
+                s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+                }
+                if (dv == (char) (s != 0 ? s + 47 : 75)) {
+                validacion = true;
+                }
+            } catch (Exception) {
+            }
+            return validacion;
+        }
+
+
+
 	}
     
 }
--- CotizacionesFinal2RROAAtesting/UnitTest1.cs
+++ CotizacionesFinal2RROAAtesting/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit; 
+using Cotizaciones.Models;
 //using CotizacionesFinal2RROAA.
 
 namespace CotizacionesFinal2RROAAtesting
@@ -9,8 +10,27 @@ namespace CotizacionesFinal2RROAAtesting
         [Fact]
         public void Test1()
         {
-            
-
         }
+
+        public void retornarExcepcionPorGuionVerificador()
+        {
+            var result = new Persona();
+            result.Rut = "89938627";
+
+            Exception ex = Assert.Throws<ArgumentException>(() => result.validate());
+
+            Assert.Equal("RUT no valido", ex.Message);
+        }
+
+         public void retornarPersonaPorRutCorrecto()
+        {
+            var prueba = new Persona();
+            prueba.Rut = "8993862-7";
+            var esperado = new Persona();
+            esperado = prueba.validate();
+            ///Si la persona retornada es la misma que la que se ingreso 
+            Assert.Equal(prueba,esperado);
+        }
+
     }
 }
